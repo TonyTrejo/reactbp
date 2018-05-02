cb71d6b531e1e09da6095ea6bca49c7adfea55de
tonytrejo <you@example.com>
2018-05-02 12:51:54 -0700

    app initialized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import SlideShow from './components/Carousel/Carousel.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <SlideShow/>
+    );
+  }
+}
+
+export default App;
